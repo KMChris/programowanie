e6dfc97209d32918c499ec4780b6c4b4ed8de370
Maciej Kosierb <262239@student.pwr.edu.pl>
2023-01-11 16:35:09 +0100

    Add import

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Download stock market data on commodities from the Yahoo Finance website
 driver = webdriver.Chrome()
