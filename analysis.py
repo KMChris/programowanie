01b3d5d8c6d6330fb26e3f704d108e5dcdd8b7e6
Wiktoria Kudła <262254@student.pwr.edu.pl>
2023-01-18 18:45:41 +0100

    add stochastic oscillator

--- analysis.py
+++ analysis.py
@@ -17,3 +17,31 @@ def EMA(data, n):
     return ema[0]
 
 print(EMA(data, 10))
+
+import pandas as pd
+
+
+def stochastic_oscillator(data, n=14):
+    """
+    Calculates the stochastic oscillator for the given data.
+    :param data: pandas DataFrame of historical data with the column 'close'
+    :param n: The number of periods for the %K line.
+    :return: A tuple of two lists, %K and %D lines.
+    """
+    data=data[::-1]
+
+    # Find the highest high and lowest low for the last n periods
+    highest_high = data['close'].rolling(n).max()
+    lowest_low = data['close'].rolling(n).min()
+
+    # Calculate the %K line
+    k = 100 * (data['close'] - lowest_low) / (highest_high - lowest_low)
+
+    # Calculate the %D line
+    d = k.rolling(n).mean()
+
+    return k[0], d[0]
+
+k,d = stochastic_oscillator(data)
+print(k)
+print(d)
