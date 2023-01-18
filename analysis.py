87c231e717eb0d022619b9b65395cff9364e7128
Krzysztof Mizgała <km.2001@o2.pl>
2023-01-18 20:58:23 +0100

    Implement MACD, RSI and Stochastic Oscillator

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ class Analysis:
         self.symbol = symbol
         self.data = self.api.get_historical(symbol, interval)
 
-    def SMA(self, period: int) -> pd.DataFrame:
+    def sma(self, period: int) -> pd.DataFrame:
         """
         Simple Moving Average (SMA) is the most basic
         type of moving average. It is calculated by
@@ -26,10 +26,9 @@ class Analysis:
         pandas.DataFrame
             Dataframe with SMA
         """
-        return self.data['close'].rolling(
-            period).mean()
+        return self.data['close'].rolling(period).mean()
 
-    def EMA(self, period: int) -> pd.DataFrame:
+    def ema(self, period: int) -> pd.DataFrame:
         """
         Exponential Moving Average (EMA) is a type of
         moving average that places a greater weight and
@@ -52,12 +51,132 @@ class Analysis:
         return self.data['close'].ewm(
             span=period, adjust=False).mean()
 
-print(SMA(data, 10))
+    def macd(self, signal_period: int = 9, slow_period: int = 12,
+             fast_period: int = 26) -> pd.DataFrame:
+        """
+        Moving Average Convergence Divergence (MACD)
+        is a trend-following momentum indicator that
+        shows the relationship between two moving
+        averages of prices.
 
-# Exponential Moving Average
-def EMA(data, n):
-    data = data[:n]['close']
-    ema = data.ewm(com=0.4).mean()
-    return ema[0]
+        Parameters
+        ----------
+        signal_period : int
+            Number of periods for signal EMA
+        slow_period : int
+            Number of periods for slow EMA
+        fast_period : int
+            Number of periods for fast EMA
 
-print(EMA(data, 10))
+        Returns
+        -------
+        pandas.DataFrame
+            Dataframe with MACD
+        """
+        diff = self.ema(fast_period) - self.ema(slow_period)
+        signal = diff.ewm(span=signal_period, adjust=False).mean()
+        hist = diff - signal
+        return pd.DataFrame({
+            'MACD': diff,
+            'Signal': signal,
+            'Histogram': hist
+        })
+
+    def rsi(self, period: int = 14) -> pd.DataFrame:
+        """
+        Relative Strength Index (RSI) is a momentum
+        indicator that measures the magnitude of recent
+        price changes to evaluate overbought or
+        oversold conditions in the price of a stock
+        or other asset.
+
+        Parameters
+        ----------
+        period : int
+            Number of periods to calculate RSI
+
+        Returns
+        -------
+        pandas.DataFrame
+            Dataframe with RSI
+        """
+        delta = self.data['close'].diff()
+        up, down = delta.copy(), delta.copy()
+        up[up < 0] = 0
+        down[down > 0] = 0
+        roll_up = up.rolling(period).mean()
+        roll_down = down.abs().rolling(period).mean()
+        rs = roll_up / roll_down
+        return 100 - (100 / (1 + rs))
+
+    def stochastic(self, period_k: int = 14, period_d: int = 3,
+                   period_s: int = 3) -> pd.DataFrame:
+        """
+        Stochastic Oscillator is a momentum indicator
+        comparing a particular closing price to a range
+        of its prices over a certain period of time.
+
+        Parameters
+        ----------
+        period_k : int
+            Number of periods to calculate %K
+        period_d : int
+            Number of periods to calculate %D
+        period_s : int
+            Number of periods to calculate SMA
+
+        Returns
+        -------
+        pandas.DataFrame
+            Dataframe with stochastic
+        """
+        high = self.data['high'].rolling(period_k).max()
+        low = self.data['low'].rolling(period_k).min()
+        k = 100 * (self.data['close'] - low) / (high - low)
+        d = k.rolling(period_d).mean()
+        return pd.DataFrame({
+            '%K': k,
+            '%D': d.rolling(period_s).mean()
+        })
+
+    # TODO Add more indicators:
+    # Money Flow Index and Ratio
+    # Chaikin Oscillator
+    # Accumulation/Distribution
+    # True Strength Index
+    # KST Oscillator
+    # Vortex Indicator: http://www.vortexindicator.com/VFX_VORTEX.PDF
+    # Mass Index
+    # Trix
+    # Standard Deviation
+    # Donchian Channel
+    # Ultimate Oscillator
+    # Keltner Channel
+    # Coppock Curve
+    # Ease of Movement
+    # Force Index
+    # On Balance Volume
+    # Commodity Channel Index
+    # Average Directional Index
+    # Bollinger Bands
+    # Parabolic SAR
+    # Ichimoku Cloud
+    # Chande Momentum Oscillator
+    # Detrended Price Oscillator
+    # Elder-Ray Index
+    # Rate of Change
+    # Stochastic RSI
+    # Williams %R
+    # Awesome Oscillator
+    # KAMA
+    # Hull Moving Average
+    # Volume-price Trend
+    # Volume-weighted Moving Average
+    # Volume Oscillator
+
+
+if __name__ == "__main__":
+    print("This is a module with technical indicators for financial data.")
+    analysis = Analysis('AAPL')
+    print(analysis.sma(10))
+    print(analysis.ema(10))
