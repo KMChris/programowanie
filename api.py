8f946a6d2376eb17c8921b4e85395f3c253f76d9
Krzysztof Mizgała <km.2001@o2.pl>
2023-01-18 01:08:21 +0100

    Write api.py objectively

--- api.py
+++ api.py
@@ -1,26 +1,50 @@
-import requests
+from dotenv import load_dotenv
+import pandas as pd
+import requests, os
 
+load_dotenv()
+API_URL = "https://financialmodelingprep.com/api/"
+API_KEY = os.environ.get("API_KEY")
 
-API_URL = "https://financialmodelingprep.com/api/v3/"
+class Stock:
+    """
+    Stock class
+    """
 
-def get_json(url):
-    """
-    Get json from url
-    """
-    url = API_URL + url + '?apikey='
-    return requests.get(url).json()
+    @staticmethod
+    def _get_json(url):
+        """
+        Get json from url
+        """
+        url = API_URL + url + '?apikey=' + API_KEY
+        return requests.get(url).json()
 
-def list_stocks():
-    """
-    List all stocks
-    """
-    url = 'stock/list'
-    return get_json(url)
+    @classmethod
+    def list_stocks(cls):
+        """
+        List all stocks
+        """
+        url = API_URL + 'v3/stock/list' + '?apikey=' + API_KEY
+        return requests.get(url).iter_content()
 
-def get_historical_price(symbol):
-    """
-    Get historical price for
-    a specific stock symbol
-    """
-    url = 'historical-price-full/' + symbol
-    return get_json(url)['historical']
+    @classmethod
+    def get_historical_price(cls, symbol):
+        """
+        Get historical price for
+        a specific stock symbol
+        """
+        url = 'v3/historical-price-full/' + symbol
+        return cls._get_json(url)['historical']
+
+    @classmethod
+    def get_historical_capitalization(cls, symbol):
+        """
+        Get historical capitalization for
+        a specific stock symbol
+        """
+        url = 'v3/historical-market-capitalization/{symbol}'
+        return cls._get_json(url)['historical']
+
+stock = Stock()
+print(stock.list_stocks())
+print(stock.get_historical_price('AAPL'))
