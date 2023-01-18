6da84eb7e04ddf8475e63083eed686b45fe6659d
Krzysztof Mizgała <km.2001@o2.pl>
2023-01-18 23:52:11 +0100

    Add plot_candles() method in ui.py

--- ui.py
+++ ui.py
@@ -1,10 +1,10 @@
+from matplotlib import ticker, pyplot as plt
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout,
                              QVBoxLayout, QSpacerItem, QPushButton,
                              QSizePolicy, QScrollArea, QLabel,
                              QTableWidget, QTableWidgetItem, QComboBox)
 from PyQt5.QtGui import QFont
 from analysis import Analysis
-from threading import Thread
 import sys
 
 
@@ -78,6 +78,7 @@ class Application(QWidget):
             ['EMA', analysis.ema()[0]],
             ['RSI', analysis.rsi()[0]]
         ])
+        self.plot_candles(analysis.data)
 
     def display_data(self, data):
         """
@@ -89,6 +90,67 @@ class Application(QWidget):
         for i, row in enumerate(data):
             for j, item in enumerate(row):
                 self.output.setItem(i, j, QTableWidgetItem(str(item)))
+        self.output.resizeColumnsToContents()
+
+    @staticmethod
+    def plot_candles(data):
+        """
+        Plot candlestick chart
+        """
+        o, h, c, l, v = data['open'], data['high'], data['close'], data['low'], data['volume']
+        scale = 'linear'  # scale = 'log'
+        points = None
+        plt.style.use('dark_background')
+        plt.rcParams['axes.linewidth'] = 0.5
+        plt.rcParams['figure.facecolor'] = '#131722'
+        plt.gca().set_facecolor('#131722')
+        highest, lowest, max_volume = h[0], l[0], v[0]
+        for k in range(len(o)):
+            if h[k] > highest:
+                highest = h[k]
+            if l[k] < lowest:
+                lowest = l[k]
+            if v is not None:
+                if v[k] > max_volume:
+                    max_volume = v[k]
+        for k in range(len(o)):
+            if c[k] >= o[k]:
+                plt.gca().add_patch(plt.Rectangle(
+                    (k + 1, o[k]), 0.618, (c[k] - o[k]),
+                    color='#2BA59A'))
+                plt.gca().add_line(plt.Line2D(
+                    [k + 1.309, k + 1.309], [l[k], h[k]],
+                    color='#2BA59A', linewidth=1))
+                if v is not None:
+                    plt.axvspan(k + 0.809, k + 1.809,
+                                ymax=(v[k] / max_volume / 6.18),
+                                facecolor='#2BA59A', alpha=0.5)
+            else:
+                plt.gca().add_patch(plt.Rectangle(
+                    (k + 1, o[k]), 0.618, (c[k] - o[k]),
+                    color='#EF5350'))
+                plt.gca().add_line(plt.Line2D(
+                    [k + 1.309, k + 1.309], [l[k], h[k]],
+                    color='#EF5350', linewidth=1))
+                if v is not None:
+                    plt.axvspan(k + 0.809, k + 1.809,
+                                ymax=(v[k] / max_volume / 6.18),
+                                facecolor='#EF5350', alpha=0.5)
+        if points is not None:
+            plt.scatter(*points, color='orange', alpha=1)
+        plt.yscale(scale)
+        if lowest - 0.05 * (highest - lowest) < 0:
+            plt.gca().set_ylim([0, highest + 0.05 * (highest - lowest)])
+        else:
+            plt.gca().set_ylim([lowest - 0.05 * (highest - lowest),
+                                highest + 0.05 * (highest - lowest)])
+        plt.gca().set_xlim([0, len(o) + 2])
+        plt.gca().grid(True, color='#38414E', linewidth=0.5)
+        plt.gca().set_axisbelow(True)
+        plt.subplots_adjust(left=0.1, right=1, top=1, bottom=0.05)
+        plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=15))
+        plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(nbins=30))
+        plt.show()
 
 
 if __name__ == "__main__":
