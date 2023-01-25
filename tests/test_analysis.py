3f1e3cbea3fcb01900d6dcb323b99b535fa179de
Maciej Kosierb <262239@student.pwr.edu.pl>
2023-01-25 12:56:22 +0100

    Add sma test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,3 +2,9 @@ import pandas as pd
 from src.analysis import Analysis
 
 analysis = Analysis('AAPl')
+
+
+def test_sma():
+    analysis.data = pd.DataFrame({'close': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+    analysis.sma(10)
+    assert analysis.sma(10)[9] == 5.5
